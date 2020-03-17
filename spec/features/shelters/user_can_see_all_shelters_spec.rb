6bd8c401855d81e950a6f9857a217d632d15deac
David Tran <davidttemail@gmail.com>
2020-03-17 16:21:33 -0600

    Add tests for index and an individual shelter

---
+++ spec/features/shelters/user_can_see_all_shelters_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "shelters index page" do
+  it "can display all shelters" do
+    shelter_1 = Shelter.create(name:    "Dumb Friends League",
+                               address: "123 Fake Street",
+                               city:    "Castle Rock",
+                               state:   "CO",
+                               zip:     "80104")
+    shelter_2 = Shelter.create(name:    "Pawty Please",
+                               address: "000 Another Fake St",
+                               city:    "Denver",
+                               state:   "CO",
+                               zip:     "80204")
+    visit "/shelters"
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_link("Dumb Friends League")
+    expect(page).to have_content(shelter_2.name)
+    expect(page).to have_link("Pawty Please")
+    expect(page).to have_link("New Shelter", href:"shelters/new")
+  end
+end
