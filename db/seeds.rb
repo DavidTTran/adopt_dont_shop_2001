90f945550b5c5ab02aeef61e2c37874ae6dfec77
David Tran <davidttemail@gmail.com>
2020-03-19 15:16:22 -0600

    Add pet seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Shelter.create(name: "Dumb Friends League", address: "123 Fake St", city: "Castle Rock", state: "CO", zip: "80104")
 Shelter.create(name: "Pawty Please", address: "000 Another Fake St", city: "Denver", state: "CO", zip: "80204")
+
+Pet.create(name: "Buddy", approximate_age: 6, sex: "male", image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/single-minded-royalty-free-image-997141470-1558379890.jpg?crop=0.671xw:1.00xh;0.0847xw,0&resize=640:*")
+
+Pet.create(name: "King", approximate_age: 5, sex: "male", image: "https://www.espree.com/sites/default/files/2019-10/DobermanPinscher.png")
