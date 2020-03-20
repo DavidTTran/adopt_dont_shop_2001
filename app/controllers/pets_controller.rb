f422740b994ff096ac87e5ea797fe9763032902c
David Tran <davidttemail@gmail.com>
2020-03-20 10:49:04 -0600

    Add MVC for updating pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -18,6 +18,16 @@ class PetsController < ApplicationController
     redirect_to ("/shelters/#{@shelter.id}/pets")
   end
 
+  def edit
+    @pet = Pet.find(params[:pet_id])
+  end
+
+  def update
+    @pet = Pet.find(params[:pet_id])
+    @pet.update(pet_params_new)
+    redirect_to "/pets/#{@pet.id}"
+  end
+
   private
   def pet_params_new
     params[:adoption_status] = "Adoptable"
