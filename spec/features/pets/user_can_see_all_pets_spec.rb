73bf1d19eb1e6f024fcb65bd43465fc82e196075
David Tran <davidttemail@gmail.com>
2020-03-19 18:10:42 -0600

    Add test for viewing pets index

--- spec/features/pets/user_can_see_all_pets_spec.rb
+++ spec/features/pets/user_can_see_all_pets_spec.rb
@@ -9,17 +9,24 @@ describe "pets index page" do
                                state:   "CO",
                                zip:     "80104")
 
-    pet_1 = Pet.create(name: "Buddy",
+    pet_1 = shelter_1.pets.create(name: "Buddy",
                        approximate_age: 6,
-                       sex: "male",
+                       sex: "Male",
                        image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/single-minded-royalty-free-image-997141470-1558379890.jpg?crop=0.671xw:1.00xh;0.0847xw,0&resize=640:*")
-    pet_2 = Pet.create(name: "King",
+
+    pet_2 = shelter_1.pets.create(name: "King",
                        approximate_age: 5,
-                       sex: "male",
+                       sex: "Male",
                        image: "https://www.espree.com/sites/default/files/2019-10/DobermanPinscher.png")
 
     visit "/pets"
 
-    expect(page).to have_content("Buddy")
+    expect(page).to have_content(pet_1.name)
+    expect(page).to have_content(pet_2.name)
+    expect(page).to have_content(pet_2.approximate_age)
+    expect(page).to have_content(pet_1.sex)
+    expect(page).to have_css("img[src*='#{pet_1.image}']")
+    expect(page).to have_content(shelter_1.name)
+
   end
 end
