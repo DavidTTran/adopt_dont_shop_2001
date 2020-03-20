b95c8a4bebad1ce01fab7937c3b00091a729c533
David Tran <davidttemail@gmail.com>
2020-03-19 18:50:03 -0600

    Add additional tests for pets index

--- spec/features/pets/user_can_see_all_pets_spec.rb
+++ spec/features/pets/user_can_see_all_pets_spec.rb
@@ -22,11 +22,11 @@ describe "pets index page" do
     visit "/pets"
 
     expect(page).to have_content(pet_1.name)
-    expect(page).to have_content(pet_2.name)
-    expect(page).to have_content(pet_2.approximate_age)
     expect(page).to have_content(pet_1.sex)
     expect(page).to have_css("img[src*='#{pet_1.image}']")
     expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(pet_2.name)
+    expect(page).to have_content(pet_2.approximate_age)
 
   end
 end
