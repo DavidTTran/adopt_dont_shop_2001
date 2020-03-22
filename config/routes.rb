7ef9a7040b5d1d8097a224dc8cd66df7e6c3b1f4
David Tran <davidttemail@gmail.com>
2020-03-22 15:03:28 -0600

    Add MVC for deleting a pet from shelter pets index

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/shelters/:id", to: "shelters#show"
   get "/shelters/:id/edit", to: "shelters#edit"
   patch "/shelters/:id", to: "shelters#update"
+  delete "/shelters/:shelter_id/pets/:pet_id", to: "shelter_pets#destroy"
   delete "/shelters/:id", to: "shelters#destroy"
 
 
